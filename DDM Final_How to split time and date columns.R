182501edb1b6f14872d692b34a1df41b8bfea6bd
Francisco Santamarina <fjsantam@gmail.com>
2016-11-29 17:07:32 -0500

    Update DDM Final_How to split time and date columns.R
    More information on gzcon and decompressing .RDS from a website: https://mgimond.github.io/ES218/Week02b.html

--- DDM Final_How to split time and date columns.R
+++ DDM Final_How to split time and date columns.R
@@ -1,20 +1,32 @@
 # The code below is used to separate two columns in the provided dataset 
-## starttime is split into start.date and start.time
-## start.date returne a date in the MM/DD/YYYY format
-## This dataset is only for January 2015, so the data will be 1/DD/2015
-## start.time returns time in the HH:MM format.
+### starttime is split into start.date and start.time
+### start.date returne a date in the MM/DD/YYYY format
+### This dataset is only for January 2015, so the data will be 1/DD/2015
+### start.time returns time in the HH:MM format.
 
 # The same functions and order are applied to stoptime to generate stop.date and stop.time
 
+library( dplyr )
+library( tidyr )
 
-dat <- readRDS("C:/Users/franc/Documents/Graduate School/PAI 730 Data-Driven Mgmt/Final Project/NYC Bike Share Data.rds")
-dat <- readRDS("https://raw.github.com/fjsantam/Maxwell-DDM-Final-Project/blob/master/NYC%20Bike%20Share%20Data.rds?raw=true")
-
+dat <- readRDS(
+        gzcon(
+          url(
+          "https://cdn.rawgit.com/fjsantam/Maxwell-DDM-Final-Project/master/NYC%20Bike%20Share%20Data.rds"
+            )
+          )
+        )
+# gzcon is a built-in decompressor that must be run when loading a .RDS straight from a website.
+# more info here: https://mgimond.github.io/ES218/Week02b.html
 
 
 dat <- dat %>% separate( starttime, c("start.date", "start.time"), sep = " " )
 dat <- dat %>% separate( stoptime, c("stop.date", "stop.time"), sep = " " )
 
+# Confirm that the  dataframe has the correct new columns
+head( dat )
+
 #To write to a CSV, use the code below.
 write.csv( dat, "C:/Users/...File Name.csv", row.names = F)
 # Put the file path here that you want the .csv to be written to.
+# Writing this file to .csv returns a file ~50MB in size, so this action is not recommended.
